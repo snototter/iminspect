8f4ed2b9c7a7af84035d3610152d10390ced61db
snototter <h.possegger+dev@gmail.com>
2019-12-30 00:50:23 +0100

    added install dependencies

--- python/setup.py
+++ python/setup.py
@@ -13,7 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/snototter/iminspect",
     packages=setuptools.find_packages(),
-    license_file='../LICENSE',
+    install_requires=['numpy', 'Pillow', 'PyQt5', 'qimage2ndarray'],
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
