ee69ef96e00a1ee21dc98aa2452cb23f5e0472f2
snototter <h.possegger+dev@gmail.com>
2020-01-03 21:25:16 +0100

    bump vito version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
         'numpy',
         'PyQt5',
         'qimage2ndarray',
-        'vito>=1.0.1'],
+        'vito>=1.0.2'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
